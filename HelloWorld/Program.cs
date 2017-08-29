0bd3a293f51e072644955a61e15b3ef4881da10d
Sarah <admin@AR-113-04>
2017-08-29 15:58:13 +0200

    2nd check in

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Indtast navn");
+
+            string input = Console.ReadLine();
+
+            input = input.ToUpper();
+
+            Console.WriteLine("Velkommen " + input);
+
+            System.Console.Write("Tryk en tast for at fortsætte");
+
+            System.Console.ReadKey();
+
+        }
+    }
+}
--- Module03_ValueTypes/Program.cs
+++ Module03_ValueTypes/Program.cs
@@ -1,9 +1,29 @@
-﻿namespace Module03_ValueTypes
+﻿using System;
+
+namespace Module03_ValueTypes
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            //System.Int32 i;
+            //System.Decimal komma;
+            //System.Boolean b;
+
+            int i; // normal brugt
+            decimal komma; // normal brugt
+            double d;
+            long l;
+            DateTime dato; // normal brugt
+            bool b; // normal brugt
+
+            i = 0;
+            i = i + 1;
+
+            dato = DateTime.Now;
+            DateTime jul = new DateTime(2017, 12, 24);
+            System.TimeSpan ts = jul.Subtract(dato);
 
             if (System.Diagnostics.Debugger.IsAttached)
             {
