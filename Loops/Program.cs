792f51d6ad2b4465492df9498d8e71f333d84d3b
Sarah <admin@AR-113-04>
2017-08-30 15:20:07 +0200

    .

---
+++ Loops/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            for (int i = 1; i < 11; i++)
+            {
+                for (int ii = 1; ii < 11; ii++)
+                {
+                    num = (i * ii);
+                    if (num > 50)
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                    else
+                        Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write(num.ToString().PadLeft(5));
+                }
+                Console.WriteLine();
+            }
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Console.Write("Press any key to continue . . . ");
+                System.Console.ReadKey();
+            }
+        }
+    }
+}
