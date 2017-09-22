bcd9eae782cc4d0526fdf0d5f35bdac7a1c60eaf
Sarah <admin@AR-113-04>
2017-09-22 15:40:07 +0200

    End of day 4

---
+++ Polymorphism/Dyr.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    abstract class Dyr
+    {
+        public string Navn { get; set; }
+        private static Random rnd = new Random();
+        public abstract void SigNoget();
+        //{
+        //    Console.WriteLine("Jeg er et dyr og hedder " + this.Navn);
+        //}
+
+        public static Dyr TilfældigtDyr()
+        {
+            string sti = @"X:\dyrenavne.txt";
+            string[] navne = System.IO.File.ReadAllLines(sti);
+            //for (int i = 0; i < navne.Length; i++)
+            //{
+            //    Console.WriteLine(navne[i]);
+            //}
+            int num = rnd.Next(0, navne.Length);
+            if (num % 2 == 0)
+            {
+                return new Hund() { Navn = navne[num] };
+            }
+            else
+            {
+                return new Kat() { Navn = navne[num] };
+            }
+        } 
+    }
+}
---
+++ Polymorphism/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dyr[] DyreArray = new Dyr[20];
+            for (int i = 0; i < 20; i++)
+            {
+                DyreArray[i] = Dyr.TilfældigtDyr();
+            }
+            foreach (var item in DyreArray)
+            {
+                item.SigNoget();
+            }
+
+            //Dyr D = new Dyr();
+            //D.Navn = "KERBUNCKLE";
+            //D.SigNoget();
+        }
+    }
+}
